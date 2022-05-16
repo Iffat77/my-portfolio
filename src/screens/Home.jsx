cb8d99794a7bed6855e3d73dbd4dd685781044fc
Iffat Hossain <iffathossain@iffats-air.mynetworksettings.com>
2022-05-15 21:42:12 -0400

    updates favicon and app name

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -69,7 +69,7 @@ function Home() {
        </div>
       </section>
     
-      <div className='m-2'>
+      <div className='m-2 '>
         <h1 className='text-orange-300 text-center my-20 font-bold text-2xl md:text-4xl'>Checkout some of my projects below!</h1>
       </div>
       
